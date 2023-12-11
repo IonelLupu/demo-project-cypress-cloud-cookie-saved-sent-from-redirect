83a5a97f5370b49a893d10df90b47521be042d19
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2023-12-11 23:29:01 +0200

    about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
     const cookiesList = cookies()
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div>{cookiesList.get('cookie-test').value}</div>
+            <div>cookie value: {cookiesList.get('cookie-test')?.value}</div>
             Hello world
         </main>
     )
