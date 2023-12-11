1218af90e0c4385df738e25e6ffad3ddb2d5d317
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2023-12-11 23:10:38 +0200

    about

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,19 @@
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
+const isInsideGithub = process.env.RUNNER_ENVIRONMENT === 'github-hosted'
+
 export async function middleware(request: NextRequest) {
     let response: NextResponse | undefined
 
-    let anonymousId = request.cookies.get('cookie-test')?.value
-    if (!anonymousId) {
-        response = NextResponse.redirect(request.url)
-        response.cookies.set('cookie-test', 'test value')
+    // redirecting inside Github runners gives redirection errors because Github runners, I think, can't save cookies
+    if (!isInsideGithub) {
+
+        let anonymousId = request.cookies.get('cookie-test')?.value
+        if (!anonymousId) {
+            response = NextResponse.redirect(request.url)
+            response.cookies.set('cookie-test', 'test value')
+        }
     }
 
     return response
